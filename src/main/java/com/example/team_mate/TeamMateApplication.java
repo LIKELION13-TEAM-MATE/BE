10919963c4905e5204f2c0c94822e9b14acec0fd
rhdbqls <kyb193539@naver.com>
2025-11-11 23:46:17 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/team_mate/TeamMateApplication.java
@@ -0,0 +1,13 @@
+package com.example.team_mate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamMateApplication.class, args);
+	}
+
+}
